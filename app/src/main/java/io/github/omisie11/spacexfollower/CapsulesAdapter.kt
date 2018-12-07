14dd3c13d6e62dcb85cfef259998fe8494304b79
OMIsie11 <omisie@gmail.com>
2018-12-07 20:08:25 +0100

    Add Group for views visible when item is expanded

--- app/src/main/java/io/github/omisie11/spacexfollower/CapsulesAdapter.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/CapsulesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.Group
 import androidx.recyclerview.widget.RecyclerView
 import io.github.omisie11.spacexfollower.data.model.Capsule
 
@@ -28,13 +29,14 @@ class CapsulesAdapter : RecyclerView.Adapter<CapsulesAdapter.ViewHolder>() {
         holder.capsuleType.text = mCapsulesData!![position].type
         holder.capsuleStatus.text = mCapsulesData!![position].status
         // Views below are visible when view is expanded on click
+        holder.capsuleLandings.text = "Landings: " + mCapsulesData!![position].landings.toString()
         holder.capsuleMissionsTextView.text =
                 if (mCapsulesData!![position].missions!!.isEmpty()) "No missions yet"
                 else mCapsulesData!![position].missions.toString()
 
         // Variable for storing info if view is expanded
         val isExpanded = position == mExpandedPosition
-        holder.capsuleMissionsTextView.visibility = if (isExpanded) View.VISIBLE else View.GONE
+        holder.groupExpanded.visibility = if (isExpanded) View.VISIBLE else View.GONE
         holder.itemView.isActivated = isExpanded
         holder.itemView.setOnClickListener {
             // Expand view on click
@@ -52,7 +54,9 @@ class CapsulesAdapter : RecyclerView.Adapter<CapsulesAdapter.ViewHolder>() {
         val capsuleSerialTextView: TextView = itemView.findViewById(R.id.text_capsule_serial)
         val capsuleType: TextView = itemView.findViewById(R.id.text_capsule_type)
         val capsuleStatus: TextView = itemView.findViewById(R.id.text_capsule_status)
+        val capsuleLandings: TextView = itemView.findViewById(R.id.text_landings)
         val capsuleMissionsTextView: TextView = itemView.findViewById(R.id.text_missions)
+        val groupExpanded: Group = itemView.findViewById(R.id.group_expand)
     }
 
     fun setData(data: List<Capsule>?) {
