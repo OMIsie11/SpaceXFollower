3883ebc2424b97908223a60b9efe2bd3e215ddfd
OMIsie11 <omisie@gmail.com>
2018-12-09 14:23:45 +0100

    Add fragment, move layout from activity to new fragment

---
+++ app/src/main/java/io/github/omisie11/spacexfollower/CapsulesFragment.kt
@@ -0,0 +1,61 @@
+package io.github.omisie11.spacexfollower
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.github.omisie11.spacexfollower.data.SpaceRepository
+import io.github.omisie11.spacexfollower.data.model.Capsule
+import kotlinx.android.synthetic.main.fragment_capsules.*
+import kotlinx.android.synthetic.main.fragment_capsules.view.*
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class CapsulesFragment : Fragment() {
+
+    private val viewAdapter: CapsulesAdapter by inject()
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private val repository: SpaceRepository by inject()
+    val model: CapsulesViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_capsules, container, false)
+
+        // Setup recyclerView
+        viewManager = LinearLayoutManager(activity)
+        rootView.capsulesRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // ViewModel setup
+        model.getCapsules().observe(this, Observer<List<Capsule>> { capsules ->
+            viewAdapter.setData(capsules)
+        })
+
+        // Force fetching capsules
+        fetchButton.setOnClickListener {
+            repository.fetchCapsulesAndSaveToDb()
+        }
+        // Delete data from capsules table
+        deleteEntriesButton.setOnClickListener {
+            repository.deleteAllCapsules()
+        }
+    }
+}
--- app/src/main/java/io/github/omisie11/spacexfollower/MainActivity.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/MainActivity.kt
@@ -2,47 +2,15 @@ package io.github.omisie11.spacexfollower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import io.github.omisie11.spacexfollower.data.SpaceRepository
-import io.github.omisie11.spacexfollower.data.model.Capsule
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewAdapter: CapsulesAdapter by inject()
-    private lateinit var viewManager: RecyclerView.LayoutManager
-    private val repository: SpaceRepository by inject()
-    val model: CapsulesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewManager = LinearLayoutManager(this)
-        capsulesRecyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
-        }
 
-
-        //val model = ViewModelProviders.of(this).get(CapsulesViewModel::class.java)
-        model.getCapsules().observe(this, Observer<List<Capsule>>{capsules ->
-            viewAdapter.setData(capsules)
-        })
-
-        // Force fetching data
-        fetchButton.setOnClickListener {
-            repository.fetchCapsulesAndSaveToDb()
-        }
-
-        // Delete data from capsules table
-        deleteEntriesButton.setOnClickListener {
-            repository.deleteAllCapsules()
-        }
     }
 }
