c7baadb680346df28883939cfd082a62fe01ce91
omisie <omisie@gmail.com>
2019-02-23 17:15:33 +0100

    Implement blank splash screen

--- app/src/main/java/io/github/omisie11/spacexfollower/ui/MainActivity.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/ui/MainActivity.kt
@@ -16,6 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
