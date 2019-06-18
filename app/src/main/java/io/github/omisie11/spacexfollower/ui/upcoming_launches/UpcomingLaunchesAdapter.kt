bf0af2ecbe42941bb817335dc395c31aa3392d53
OMIsie <omisie@gmail.com>
2019-06-18 15:12:11 +0200

    Show initial data in upcoming launches fragment

--- app/src/main/java/io/github/omisie11/spacexfollower/ui/upcoming_launches/UpcomingLaunchesAdapter.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/ui/upcoming_launches/UpcomingLaunchesAdapter.kt
@@ -3,9 +3,11 @@ package io.github.omisie11.spacexfollower.ui.upcoming_launches
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.github.omisie11.spacexfollower.R
 import io.github.omisie11.spacexfollower.data.model.UpcomingLaunch
+import kotlinx.android.synthetic.main.upcoming_launches_recycler_item.view.*
 
 
 class UpcomingLaunchesAdapter : RecyclerView.Adapter<UpcomingLaunchesAdapter.ViewHolder>() {
@@ -14,27 +16,25 @@ class UpcomingLaunchesAdapter : RecyclerView.Adapter<UpcomingLaunchesAdapter.Vie
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
         LayoutInflater.from(parent.context)
-            .inflate(R.layout.capsules_recycler_item, parent, false)
+            .inflate(R.layout.upcoming_launches_recycler_item, parent, false)
     )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Bind data to views from ViewHolder
         mLaunchesData.let {
-        //    holder.capsuleSerialTextView.text = mCapsulesData[position].capsuleSerial
-          //  holder.capsuleLaunchTextView.text = if (mCapsulesData[position].originalLaunch.isNullOrEmpty())
-          //      "No launch date info" else mCapsulesData[position].originalLaunch
-          //  holder.capsuleStatusTextView.text = mCapsulesData[position].status
-          //  holder.capsuleTypeTextView.text = mCapsulesData[position].type
+            holder.flightNumberTextView.text = mLaunchesData[position].flightNumber.toString()
+            holder.launchDateTextView.text = if (mLaunchesData[position].launchDateUnix.toString().isNullOrEmpty())
+                "No launch date info" else mLaunchesData[position].launchDateUnix.toString()
+            holder.missionNameTextView.text = mLaunchesData[position].missionName
         }
     }
 
     override fun getItemCount(): Int = mLaunchesData.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-      //  val capsuleSerialTextView: TextView = itemView.text_capsule_serial
-     //   val capsuleLaunchTextView: TextView = itemView.text_capsule_launch
-     //   val capsuleStatusTextView: TextView = itemView.text_capsule_status
-     //   val capsuleTypeTextView: TextView = itemView.text_capsule_type
+        val flightNumberTextView: TextView = itemView.text_flight_number
+        val launchDateTextView: TextView = itemView.text_launch_date
+        val missionNameTextView: TextView = itemView.text_mission_name
     }
 
     fun setData(data: List<UpcomingLaunch>) {
--- app/src/main/java/io/github/omisie11/spacexfollower/ui/upcoming_launches/UpcomingLaunchesFragment.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/ui/upcoming_launches/UpcomingLaunchesFragment.kt
@@ -2,16 +2,21 @@ package io.github.omisie11.spacexfollower.ui.upcoming_launches
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
-
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
 import io.github.omisie11.spacexfollower.R
 import io.github.omisie11.spacexfollower.data.model.UpcomingLaunch
+import io.github.omisie11.spacexfollower.util.OnItemClickListener
+import io.github.omisie11.spacexfollower.util.addOnItemClickListener
+import kotlinx.android.synthetic.main.fragment_recycler.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -32,23 +37,23 @@ class UpcomingLaunchesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_upcoming_launches, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_recycler, container, false)
 
         // Setup recyclerView
         viewManager = LinearLayoutManager(activity)
-       // val recyclerView = rootView.findViewById<RecyclerView>(R.id.recyclerView)
-       // recyclerView.apply {
-       //     addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
-       //     setHasFixedSize(true)
-       //     layoutManager = viewManager
-       //     adapter = viewAdapter
-       // }
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.apply {
+            addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
 
-        //   val swipeRefreshLayout = rootView.findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
-        //  swipeRefreshLayout.apply {
-        //      setProgressBackgroundColorSchemeResource(R.color.colorSurface)
-        //      setColorSchemeResources(R.color.colorSecondary)
-        // }
+        val swipeRefreshLayout = rootView.findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
+        swipeRefreshLayout.apply {
+            setProgressBackgroundColorSchemeResource(R.color.colorSurface)
+            setColorSchemeResources(R.color.colorSecondary)
+        }
 
         return rootView
     }
@@ -63,35 +68,38 @@ class UpcomingLaunchesFragment : Fragment() {
 
         // Observe if data is refreshing and show/hide loading indicator
         viewModel.getLaunchesLoadingStatus().observe(viewLifecycleOwner, Observer<Boolean> { areLaunchesRefreshing ->
-            // swipeRefreshLayout.isRefreshing = areLaunchesRefreshing
+            swipeRefreshLayout.isRefreshing = areLaunchesRefreshing
         })
 
         // Show a snackbar whenever the [ViewModel.snackbar] is updated a non-null value
-        //  viewModel.snackbar.observe(this, Observer { text ->
-        //     text?.let {
-        //        Snackbar.make(swipeRefreshLayout, text, Snackbar.LENGTH_LONG).setAction(
-        //           getString(R.string.snackbar_action_retry), View.OnClickListener {
-        //              viewModel.refreshUpcomingLaunches()
-        //         }).show()
-        //    viewModel.onSnackbarShown()
-        //}
-        //})
+        viewModel.snackbar.observe(this, Observer { text ->
+            text?.let {
+                Snackbar.make(swipeRefreshLayout, text, Snackbar.LENGTH_LONG).setAction(
+                    getString(R.string.snackbar_action_retry), View.OnClickListener {
+                        viewModel.refreshUpcomingLaunches()
+                    }).show()
+                viewModel.onSnackbarShown()
+            }
+        })
 
         // Swipe to refresh
-        //   swipeRefreshLayout.setOnRefreshListener {
-        //      Log.i("CapsulesFragment", "onRefresh called from SwipeRefreshLayout")
-        //      viewModel.refreshCapsules()
-        // }
+        swipeRefreshLayout.setOnRefreshListener {
+            Log.i("UpcomingLFragment", "onRefresh called from SwipeRefreshLayout")
+            viewModel.refreshUpcomingLaunches()
+        }
 
         // Respond to user clicks on recyclerView items
-        // recyclerView.addOnItemClickListener(object : OnItemClickListener {
-        //     override fun onItemClicked(position: Int, view: View) {
-        //        findNavController().navigate(
-        //           CapsulesFragmentDirections
-        //              .actionCapsulesDestToCapsuleDetailDest(position)
-        //    )
-        //}
-        //})
+        recyclerView.addOnItemClickListener(object : OnItemClickListener {
+            override fun onItemClicked(position: Int, view: View) {
+                // ToDo: Add detail fragment and navigate
+                //findNavController().navigate(
+                //CapsulesFragmentDirections
+                //    .actionCapsulesDestToCapsuleDetailDest(position)
+
+                //)
+                Toast.makeText(activity, "Item $position clicked!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onResume() {
