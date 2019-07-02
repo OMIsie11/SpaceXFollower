8de8b56f2b46d16fe128a4eaea482b2765cd7a37
OMIsie <omisie@gmail.com>
2019-07-02 13:02:41 +0200

    Add espresso test for checking if settings screen is opening with prefs

--- app/src/androidTest/java/io/github/omisie11/spacexfollower/NavDrawerEspressoTests.kt
+++ app/src/androidTest/java/io/github/omisie11/spacexfollower/NavDrawerEspressoTests.kt
@@ -1,13 +1,15 @@
 package io.github.omisie11.spacexfollower
 
 import android.view.Gravity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
 import androidx.test.espresso.contrib.NavigationViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
@@ -74,5 +76,33 @@ class NavDrawerEspressoTests {
         // (swipeRefreshLayout is root in that fragment)
         onView(withId(R.id.swipeRefreshLayout))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickOnNavDrawerOpenSettingsFragment() {
+        onView(withId(R.id.drawer_layout))
+            .check(matches(isClosed(Gravity.START)))
+            .perform(DrawerActions.open())
+
+        onView(withId(R.id.navigation_view))
+            .perform(NavigationViewActions.navigateTo(R.id.settings_dest))
+
+        // Check if settings are present by trying to click on each one
+        // RecyclerView actions are used because normal approach is not working with preferences from support lib
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                    hasDescendant(withText(R.string.settings_dark_mode_title)),
+                    click()
+                )
+            )
+
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                    hasDescendant(withText(R.string.settings_refresh_interval_title)),
+                    click()
+                )
+            )
     }
 }
