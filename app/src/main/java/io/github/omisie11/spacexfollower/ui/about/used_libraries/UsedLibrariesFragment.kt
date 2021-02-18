5773e10bd27fbdb86f6f730614f2471343df0e48
GeorgCantor <cupsman1986@gmail.com>
2021-02-18 23:07:57 +0300

    Moved getUsedLibraries() fun to viewModel

--- app/src/main/java/io/github/omisie11/spacexfollower/ui/about/used_libraries/UsedLibrariesFragment.kt
+++ app/src/main/java/io/github/omisie11/spacexfollower/ui/about/used_libraries/UsedLibrariesFragment.kt
@@ -3,38 +3,18 @@ package io.github.omisie11.spacexfollower.ui.about.used_libraries
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import io.github.omisie11.spacexfollower.R
 import kotlinx.android.synthetic.main.fragment_used_libraries.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class UsedLibrariesFragment : Fragment(), UsedLibrariesAdapter.OnItemClickListener {
+class UsedLibrariesFragment : Fragment(R.layout.fragment_used_libraries),
+    UsedLibrariesAdapter.OnItemClickListener {
 
     private lateinit var viewAdapter: UsedLibrariesAdapter
     private val viewModel: UsedLibrariesViewModel by viewModel()
-    private val usedLibsJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + usedLibsJob)
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_used_libraries, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,15 +22,7 @@ class UsedLibrariesFragment : Fragment(), UsedLibrariesAdapter.OnItemClickListen
         viewAdapter = UsedLibrariesAdapter(this)
         recyclerView_libs.apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(activity)
             adapter = viewAdapter
-        }
-
-        uiScope.launch {
-            val usedLibsList = async(Dispatchers.Default) {
-                getUsedLibraries()
-            }
-            viewModel.setUsedLibs(usedLibsList.await())
         }
 
         viewModel.getUsedLibs().observe(viewLifecycleOwner, Observer { librariesList ->
@@ -58,24 +30,8 @@ class UsedLibrariesFragment : Fragment(), UsedLibrariesAdapter.OnItemClickListen
         })
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        recyclerView_libs.adapter = null
-
-        usedLibsJob.cancel()
-    }
-
     override fun onItemClicked(usedLibraryRepoUrl: String) {
         openWebUrl(usedLibraryRepoUrl)
-    }
-
-    // Move this function to ViewModel if there is safe way to access RAW res from there
-    private fun getUsedLibraries(): List<UsedLibrary> {
-        val type = object : TypeToken<List<UsedLibrary>>() {}.type
-        val objectString: String =
-            activity!!.resources.openRawResource(R.raw.used_libraries).bufferedReader()
-                .use { it.readText() }
-        return Gson().fromJson(objectString, type)
     }
 
     private fun openWebUrl(urlAddress: String) {
